7382149b1b0103b08a81625a536fdf790afbd6b6
Karthic <karthics004@gmail.com>
2025-12-04 17:52:07 +0530

    Generating Visualizations

--- src.py
+++ src.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns 
 
 #Load Dataset
 url = "https://raw.githubusercontent.com/pandas-dev/pandas/master/doc/data/titanic.csv"
@@ -18,3 +20,24 @@ df = df.drop_duplicates()
 #Filter data: Passengers in first class
 first_class = df[df["Pclass"] == 1]
 print("First Class Passengers: \n", first_class.head())
+
+# Bar Chart: Survival rate by class
+survival_by_class = df.groupby("Pclass")["Survived"].mean()
+survival_by_class.plot(kind="bar", color="green")
+plt.title("Survival Rate by Class")
+plt.ylabel("Survival Rate")
+plt.show()
+
+#Histogram: Age distribution
+sns.histplot(df["Age"], kde=True, bins=20, color="skyblue")
+plt.title("Age Distribution")
+plt.ylabel("Age")
+plt.ylabel("Frequency")
+plt.show()
+
+#Scatter Plot: Age vs Fare
+plt.scatter(df["Age"], df["Fare"], alpha=0.5, color="purple")
+plt.title("Age vs Fare")
+plt.xlabel("Age")
+plt.ylabel("Fare")
+plt.show()
